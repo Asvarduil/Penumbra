3197e07b3f9e2e28697a73ccaf2783556ff0945d
Andrew R. Gray <andrew.russel.gray@gmail.com>
2017-11-21 13:22:12 -0600

    Create PlayerDebt.cs
    Added

---
+++ Asvarduil.Penumbra/Asvarduil.Penumbra.DataCore/Models/PlayerDebt.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Asvarduil.Penumbra.DataCore.Models
+{
+    public class PlayerDebt
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public int Value { get; set; }
+        public int InterestRate { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
