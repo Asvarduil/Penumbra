1efdbd5754b37027b35b16d6be5cfcdcddb20d26
Andrew R. Gray <andrew.russel.gray@gmail.com>
2017-11-21 13:25:00 -0600

    Update Reputation.cs

--- Asvarduil.Penumbra/Asvarduil.Penumbra.DataCore/Models/Reputation.cs
+++ Asvarduil.Penumbra/Asvarduil.Penumbra.DataCore/Models/Reputation.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Asvarduil.Penumbra.DataCore.Models
 {
     public class Reputation
@@ -6,5 +8,6 @@ namespace Asvarduil.Penumbra.DataCore.Models
         public int PlayerId { get; set; }
         public int FactionId { get; set; }
         public int Reputation { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
