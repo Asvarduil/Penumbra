c82dd23466e20118269c502af6e8d7e432247172
Andrew R. Gray <andrew.russel.gray@gmail.com>
2017-11-21 13:19:46 -0600

    Create Reputation.cs
    Added

---
+++ Asvarduil.Penumbra/Asvarduil.Penumbra.DataCore/Models/Reputation.cs
@@ -0,0 +1,10 @@
+namespace Asvarduil.Penumbra.DataCore.Models
+{
+    public class Reputation
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public int FactionId { get; set; }
+        public int Reputation { get; set; }
+    }
+}
