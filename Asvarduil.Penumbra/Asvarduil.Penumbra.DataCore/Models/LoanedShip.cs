c6b39e204dbf8674c6354bfcf7144ff2ff29e972
Andrew R. Gray <andrew.russel.gray@gmail.com>
2017-11-21 13:23:54 -0600

    Create LoanedShip.cs
    Added

---
+++ Asvarduil.Penumbra/Asvarduil.Penumbra.DataCore/Models/LoanedShip.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Asvarduil.Penumbra.DataCore.Models
+{
+    public class LoanedShip
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public string ShipTemplateName { get; set; }
+        public string ShipName { get; set; }
+        public int Value { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
